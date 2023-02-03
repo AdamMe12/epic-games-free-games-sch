fe8262d972e39d8eea3a0cc4cb45c639352fc99d
AdamMe12 <al.jerdabe@gmail.com>
2023-02-03 13:27:29 +0300

    init commit

---
+++ src/main/java/com/example/pictureToBarcode/EpicGameFreeGamesApplication.java
@@ -0,0 +1,13 @@
+package com.example.pictureToBarcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicGameFreeGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicGameFreeGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pictureToBarcode/EpicGameFreeGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pictureToBarcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicGameFreeGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
